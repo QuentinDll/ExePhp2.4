7a5dbac334db83277d6c96466cc30f3b3ea7db3a
QuentinDll <quentin.delplace13@gmail.com>
2018-08-24 09:40:05 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 2</title>
+</head>
+<body>
+  <p>
+    <?php
+    // declaration de la fonction
+    function carc($name)
+    {
+      return $name;
+    }
+    echo carc('Moe');
+    ?>
+  </p>
+</body>
+</html>
